7b2061b00fac704125d9fc39b6cd27361b1558fd
Alexander <alexandr941989@gmail.com>
2021-12-04 18:07:41 +0200

    Complete part 1 of day 2.

--- src/day02/main.rb
+++ src/day02/main.rb
@@ -1,1 +1,20 @@
+input = File
+        .readlines('input', chomp: true)
 
+horizontal = 0
+depth = 0
+
+input.each do |line|
+  command = line.split(' ')
+  value = command[1].to_i
+  case command[0]
+  when 'forward'
+    horizontal += value
+  when 'down'
+    depth += value
+  else # up
+    depth -= value
+  end
+end
+
+print(horizontal * depth)
