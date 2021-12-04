e15919d77d5cc12c116d64983336b372dd6bb159
Alexander <alexandr941989@gmail.com>
2021-12-04 15:27:38 +0200

    Complete part 1 of day 1.

--- day01/main.rb
+++ day01/main.rb
@@ -1,1 +1,10 @@
-print 'Hello, world!'
+# frozen_string_literal: true
+
+input = File.readlines('input', chomp: true)
+number_input = input.map(&:to_i)
+res = 0
+number_input.each_with_index do |num, i|
+  res += 1 if i > 0 && num > number_input[i - 1]
+end
+
+print res
