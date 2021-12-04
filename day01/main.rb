cdb868c98bc1a4b4e126886523e9fcea5bc9f739
Alexander <alexandr941989@gmail.com>
2021-12-04 15:37:29 +0200

    Complete part 2 of day 1.

--- day01/main.rb
+++ day01/main.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
-input = File.readlines('input', chomp: true)
-number_input = input.map(&:to_i)
+triplet_sums = File
+               .readlines('input', chomp: true)
+               .map(&:to_i)
+               .each_cons(3)
+               .map(&:sum)
 res = 0
-number_input.each_with_index do |num, i|
-  res += 1 if i > 0 && num > number_input[i - 1]
+triplet_sums.each_with_index do |num, i|
+  res += 1 if i > 0 && num > triplet_sums[i - 1]
 end
 
 print res
